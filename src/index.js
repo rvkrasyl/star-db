e7f2b4a94e1d6ee05ec55d47baf5cabab075a10e
rvkrasyl <iamdniwe56@gmail.com>
2022-11-07 01:15:05 +0200

    Class for api request created

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("May the 4th be with you");
+class SwapiService {
+    async getResource(url) {
+        const resp = await fetch(url);
+
+        if(!resp.ok) {
+            throw new Error(`${url}, received ${resp.status}`);
+        }
+        const body = await resp.json();
+        return body;
+    }
+}
+
+getResponseBody('https://swapi.dev/api/people/146465/')
+    .then((body) => {
+        console.log(body);
+    })
+    .catch((err) => {
+        console.log('Could not fetch ',err);
+    });
