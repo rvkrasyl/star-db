cacdaa270d63f82398ff40f9cfbe994667256b5f
rvkrasyl <rvkrasyl@gmail.com>
2022-11-07 11:26:20 +0200

    api service methods added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 class SwapiService {
+_apiBase = 'https://swapi.dev/api';
+
     async getResource(url) {
-        const resp = await fetch(url);
+        const resp = await fetch(`${this._apiBase}${url}`);
 
         if(!resp.ok) {
             throw new Error(`${url}, received ${resp.status}`);
@@ -8,11 +10,39 @@ class SwapiService {
         const body = await resp.json();
         return body;
     }
+
+    async getAllPeople() {
+        const res = await this.getResource(`/people/`);
+        return res.results;
+    }
+
+    getPerson(id) {
+        return this.getResource(`/people/${id}/`);
+    }
+
+    async getAllPlanets() {
+        const res = await this.getResource(`/planets/`);
+        return res.results;
+    }
+
+    getPlanet(id) {
+        return this.getResource(`/planets/${id}/`);
+    }
+
+    async getAllStarships() {
+        const res = await this.getResource(`/starships/`);
+        return res.results;
+    }
+
+    getStarship(id) {
+        return this.getResource(`/starships/${id}/`);
+    }
 }
 
-getResponseBody('https://swapi.dev/api/people/146465/')
-    .then((body) => {
-        console.log(body);
+const swapi = new SwapiService();
+swapi.getPerson(3)
+    .then((person) => {
+        console.log(person.name);
     })
     .catch((err) => {
         console.log('Could not fetch ',err);
