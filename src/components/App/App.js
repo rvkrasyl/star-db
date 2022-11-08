3e3aafa0449e0e5f609e0ee33dfdb5a689d8494d
rvkrasyl <rvkrasyl@gmail.com>
2022-11-08 17:18:33 +0200

    refactoring

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,6 +5,7 @@ import PeoplePage from "../PeoplePage";
 import ItemList from "../ItemList";
 import PersonDetails from "../PersonDetails";
 import SwapiService from "../../services/SwapiService";
+import BodyRow from "../BodyRow";
 
 import './App.css';
 export default class App extends Component {   
@@ -18,27 +19,27 @@ export default class App extends Component {
                 <RandomPlanet />
                 <PeoplePage peopleData={this.swapiService.getAllPeople}/>
 
-                <div className="row mb2">
-                    <div className="col-md-6">
+                <BodyRow 
+                    leftElement={
                         <ItemList 
                             getData={this.swapiService.getAllPlanets}
-                            renderItem={(item) => `${item.name} (${item.diameter}km²)`} />
-                    </div>
-                    <div className="col-md-6">
+                            renderItem={(item) => `${item.name} (${item.diameter}km²)`}/>
+                    } 
+                    rightElement= {
                         <PersonDetails personId={4}/>
-                    </div>
-                </div>
+                    } 
+                />
 
-                <div className="row mb2">
-                    <div className="col-md-6">
+                <BodyRow 
+                    leftElement={
                         <ItemList 
                             getData={this.swapiService.getAllStarships}
-                            renderItem={(item) => `${item.name} (${item.costInCredits}Creds)` } />
-                    </div>
-                    <div className="col-md-6">
+                            renderItem={(item) => `${item.name} (${item.costInCredits}Creds)`}/>
+                    } 
+                    rightElement= {
                         <PersonDetails personId={8}/>
-                    </div>
-                </div>
+                    } 
+                />
             </div>
         );
     }
